254a11310454221d8e8973e413601c1130a9cce8
runLeonov <leonovcasha@gmail.com>
2023-01-26 09:46:04 +0200

    [UI] - changes for ui.

--- contentPublicationUI/src/app/titles/titles.component.ts
+++ contentPublicationUI/src/app/titles/titles.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Title} from "../title";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-titles',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./titles.component.css']
 })
 export class TitlesComponent {
+  titles: Title[] = []
 
+  constructor(private httpclient: HttpClient) {
+    this.loadTitlesList();
+    console.log(this.titles);
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  private loadTitlesList() {
+    this.httpclient.get<Title[]>('content/titles').subscribe(titles => this.titles = titles)
+  }
 }
