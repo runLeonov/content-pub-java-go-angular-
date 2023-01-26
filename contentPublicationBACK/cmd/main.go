fcdfaa4873a21ed19913ca9a294713695c5e1c14
runLeonov <leonovcasha@gmail.com>
2023-01-26 09:44:38 +0200

    [BACK-END] - added new entities for database.

--- contentPublicationBACK/cmd/main.go
+++ contentPublicationBACK/cmd/main.go
@@ -13,7 +13,7 @@ func main() {
 		Host:       "localhost",
 		DriverName: "mysql",
 		Port:       "3306",
-		DBName:     "Gorm1",
+		DBName:     "Testing",
 		Username:   "root",
 		Password:   "1111",
 		ParseTime:  "true",
