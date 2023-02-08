e311344a717ef84b327a4a983d9a0d80c212f377
runLeonov <leonovcasha@gmail.com>
2023-02-09 00:05:58 +0200

    [UI] - filter and styles

--- contentPublicationUI/src/app/titles/titles.component.ts
+++ contentPublicationUI/src/app/titles/titles.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Title} from "../title";
+import {Category, PossibleContent, Serial, Tag, Title} from "../title";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../environment";
 
@@ -11,8 +11,18 @@ import {environment} from "../environment";
 export class TitlesComponent {
   titles: Title[] = []
 
-  constructor(private httpclient: HttpClient) {
+  allPossible: PossibleContent | undefined;
+  categories: Category[] = [];
+  serials: Serial[] = [];
+  tags: Tag[] = [];
+
+  hideCats: boolean = true
+  hideTags: boolean = true
+  hideSerials: boolean = true
+
+  constructor(protected httpclient: HttpClient) {
     this.loadTitlesList();
+    this.loadContents();
   }
 
 
@@ -22,5 +32,39 @@ export class TitlesComponent {
 
   private loadTitlesList() {
     this.httpclient.get<Title[]>(`${environment.serverUrl}/titles/`).subscribe(titles => this.titles = titles)
+  }
+
+  reset() {
+    this.loadTitlesList();
+  }
+
+  filterList() {
+    this.httpclient.post<Title[]>(`${environment.serverUrl}/titles/filter`, {
+      categories: this.categories,
+      tags: this.tags,
+      serials: this.serials,
+    }).subscribe(titles => this.titles = titles);
+  }
+
+  private loadContents() {
+    this.httpclient.get<PossibleContent>(`${environment.serverUrl}/titles/content-all`).subscribe(all => this.allPossible = all);
+    this.titles = []
+  }
+
+  addToArr($event: any, obj: Category | Tag | Serial, arr: any[]) {
+    if ($event.target.checked) {
+      arr.push(obj);
+      return;
+    }
+    removeFromArr(arr, obj);
+
+    function removeFromArr(arr: any[], obj: Category | Tag | Serial) {
+      if (arr.includes(obj)) {
+        const index = arr.indexOf(obj, 0);
+        if (index > -1) {
+          arr.splice(index, 1);
+        }
+      }
+    }
   }
 }
